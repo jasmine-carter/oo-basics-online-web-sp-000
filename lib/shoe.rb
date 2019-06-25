71ca3d1b6f724bec914a532547b2aaabb924a1b7
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-25 00:53:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 # Make your shoe class here!learn spec/02_shoe_spec.rb
 class Shoe
-attr_accessor :brand, :color
+attr_accessor :brand, :color, :size, :material
 attr_reader :brand
 
   def initialize(brand)
